37673bde5908af709ea037705dbd7ff3c340b1ec
Haoyu Dong <hidaviddong@gmail.com>
2024-10-18 01:03:33 +0800

    refactor: Remove commented out code for enabling mocking

--- src/main.js
+++ src/main.js
@@ -6,17 +6,19 @@ import 'element-plus/dist/index.css';
 import ElementPlus from 'element-plus';
 
  
-async function enableMocking() {
-    if (process.env.NODE_ENV !== 'development') {
-      return
-    }
+// async function enableMocking() {
+//     if (process.env.NODE_ENV !== 'development') {
+//       return
+//     }
    
-    const { worker } = await import('./mocks/browser')
-    // `worker.start()` returns a Promise that resolves
-    // once the Service Worker is up and ready to intercept requests.
-    return worker.start()
-}
+//     const { worker } = await import('./mocks/browser')
+//     // `worker.start()` returns a Promise that resolves
+//     // once the Service Worker is up and ready to intercept requests.
+//     return worker.start()
+// }
 
-enableMocking().then(() => {
-    createApp(App).use(router).use(ElementPlus).mount('#app')
-})
+// enableMocking().then(() => {
+//     createApp(App).use(router).use(ElementPlus).mount('#app')
+// })
+
+createApp(App).use(router).use(ElementPlus).mount('#app')
