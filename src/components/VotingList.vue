3e0b9061e97156fcac9a103131fd146d65d7bab5
Haoyu Dong <hidaviddong@gmail.com>
2024-11-06 20:40:57 +0800

    refactor: Update VotingList component to handle vote list click events

--- src/components/VotingList.vue
+++ src/components/VotingList.vue
@@ -1,49 +1,14 @@
 <template>
     <div class="w-1/2 p-4">
         <ul className="space-y-2" v-if="isFinished">
-            <li v-for="vote in data.voting_lists" :key="vote.voting_id">
-                <Dialog v-model:open="open">
-                    <DialogTrigger as-child>
-                        <div className="p-2 flex flex-col items-center space-x-4">
-                            <div class="w-full flex">
-                                <h3 className="cursor-pointer text-zinc-700 mr-auto">{{ vote.voting_name }}</h3>
-                                <Badge>{{ vote.voting_date }}</Badge>
-                            </div>
-                        </div>
-                        <Separator class="mt-2" />
-                    </DialogTrigger>
-                    <DialogContent class="sm:max-w-[425px]">
-                        <DialogHeader>
-                            <DialogTitle>Voter Information</DialogTitle>
-                            <DialogDescription>
-                                Please add your id and nickname.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div class="grid gap-4 py-4">
-                            <div class="grid grid-cols-4 items-center gap-4">
-                                <Label for="userid" class="text-right">
-                                    Your ID
-                                </Label>
-                                <Input id="userid" v-model="userId" class="col-span-3" />
-                            </div>
-                            <div class="grid grid-cols-4 items-center gap-4">
-                                <Label for="username" class="text-right">
-                                    Nickname
-                                </Label>
-                                <Input id="username" v-model="nickname" class="col-span-3" />
-                            </div>
-                        </div>
-                        <DialogClose class="w-full  flex-row-reverse flex">
-                            <Button type="submit" @click="() => {
-            toast.success('Voter information saved successfully')
-            // 手动路由跳转,这里先固定跳转到路由 1
-            router.push(`/vote/1`)
-        }">
-                                Save changes
-                            </Button>
-                        </DialogClose>
-                    </DialogContent>
-                </Dialog>
+            <li v-for="vote in data.voting_lists" :key="vote.voting_id" @click="handleVoteListClick">
+                <div className="p-2 flex flex-col items-center space-x-4">
+                    <div class="w-full flex">
+                        <h3 className="cursor-pointer text-zinc-700 mr-auto">{{ vote.voting_name }}</h3>
+                        <Badge>{{ vote.voting_date }}</Badge>
+                    </div>
+                </div>
+                <Separator class="mt-2" />
             </li>
         </ul>
     </div>
@@ -51,29 +16,18 @@
 
 <script setup>
 import { toast } from 'vue-sonner'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-
 import { Separator } from '@/components/ui/separator'
 import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
 import { useFetch } from '@vueuse/core'
-import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import {API_URL} from '@/lib/utils'
+import { API_URL } from '@/lib/utils'
 
 const router = useRouter()
-const open = ref(false)
-const userId = ref('s123456')
-const nickname = ref('s123456')
+
+function handleVoteListClick() {
+    toast.success('Voter information saved successfully')
+    // 手动路由跳转,这里先固定跳转到路由 1
+    router.push(`/vote/1`)
+}
 const { data, isFinished } = useFetch(`${API_URL}/voting-lists`).get().json()
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,51 @@
-
 <template>
-    <div class="h-full justify-center items-center w-full px-24 flex flex-col">
-      <VotingList />
-    </div>
+  <div class="h-full justify-center items-center w-full px-24 flex flex-col">
+    <VotingList v-if="!!user.id" />
+
+    <Card class="w-full max-w-sm mt-16" v-else>
+      <CardHeader>
+        <CardTitle class="text-2xl">
+          Voter Information
+        </CardTitle>
+        <CardDescription>
+          Please enter your id and nickname.
+        </CardDescription>
+      </CardHeader>
+      <CardContent class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="id">Voter ID</Label>
+          <Input id="id" type='text' required v-model="voteId" />
+        </div>
+        <div class="grid gap-2">
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" required v-model="nickname" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button class="w-full" @click="handleSaveClick">
+          Save
+        </Button>
+      </CardFooter>
+    </Card>
+    <footer class="absolute bottom-4 font-geist text-sm text-gray-500">By COMP 8500SEF Group 12</footer>
+  </div>
 </template>
 
 <script setup>
-    import VotingList from '@/components/VotingList.vue'
+import VotingList from '@/components/VotingList.vue'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useStorage } from '@vueuse/core'
+import { ref } from 'vue'
+const voteId = ref('')
+const nickname = ref('')
+const user = useStorage('user', { id: '', nickname: '' })
+
+function handleSaveClick() {
+  console.log('save click')
+  user.value.id = voteId.value
+  user.value.nickname = nickname.value
+}
 </script>
