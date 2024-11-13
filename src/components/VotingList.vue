0e48cfe3ecff43933653a8d4adda1cb68c5bdee8
David Dong <hidaviddong@gmail.com>
2024-11-13 18:40:06 +0800

    feat: handle delte

--- src/components/VotingList.vue
+++ src/components/VotingList.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="w-1/2 p-4" v-if="isFinished">
         <ul className="space-y-2">
-            <li v-for="vote in data.voting_lists" :key="vote.voting_id" @click="handleVoteListClick(vote.voting_id)">
+            <li v-for="vote in data.voting_lists" :key="vote.voting_id">
                 <div className="p-2 flex flex-col items-center space-x-4">
-                    <div class="w-full flex">
-                        <h3 className="cursor-pointer text-zinc-700 mr-auto">{{ vote.voting_name }}</h3>
+                    <div class="w-full flex gap-x-2">
+                        <h3 className="cursor-pointer text-zinc-700 mr-auto hover:underline" @click="handleVoteListClick(vote.voting_id)">{{ vote.voting_name }}</h3>
                         <Badge>{{ vote.voting_date }}</Badge>
+                        <Badge class="hover:cursor-pointer" variant="destructive" v-show="vote.is_auth_delete" @click="handleDeleteClick(vote.voting_id)"> Delete </Badge>
                     </div>
                 </div>
                 <div></div>
@@ -35,5 +36,11 @@ function handleCreateVoteClick(){
     router.push("/createVote")
 }
 const user = JSON.parse(localStorage.getItem('user'))
+
+function handleDeleteClick(voteId){
+    console.log("delete vote id",voteId, "user id is",user.vote_id)
+}
+
 const { data, isFinished } = useFetch(`${API_URL}/voting-lists?user_id=${user.vote_id}`).get().json()
+
 </script>
